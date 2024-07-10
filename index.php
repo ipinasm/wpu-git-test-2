0e22b6919ac9c04757622b61fcb8629156eafaf0
ipinasm <ipinahamadsyarifudin@gmail.com>
2024-07-10 09:27:55 +0700

    menambahkan ul li di dalam fordel index.php

--- index.php
+++ index.php
@@ -5,8 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>coba git remote</title>
 </head>
-<>
+<body>
     <h1>hello world!</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem, dolore!</p>
-</>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
+</body>
 </html>
